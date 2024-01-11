d8db44e750d7497a97461b979dd74448e5f1e95e
Gisela <giselatb@outlook.com>
2024-01-11 22:35:03 +0000

    Add dotnet client for mqtt demo

---
+++ client/Program.cs
@@ -0,0 +1,43 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+using System.Security.Cryptography.X509Certificates;
+
+string hostname = "event-grid-mqtt-ns.westeurope-1.ts.eventgrid.azure.net";
+string clientId = "client1-session1";  //client ID can be the session identifier.  A client can have multiple sessions using username and clientId.
+string x509_pem = @"../client1-authnID.pem";  //Provide your client certificate .cer.pem file path
+string x509_key = @"../client1-authnID.key";  //Provide your client certificate .key.pem file path
+
+var certificate = new X509Certificate2(X509Certificate2.CreateFromPemFile(x509_pem, x509_key).Export(X509ContentType.Pkcs12));
+
+var mqttClient = new MqttFactory().CreateMqttClient();
+
+var connAck = await mqttClient!.ConnectAsync(new MqttClientOptionsBuilder()
+    .WithTcpServer(hostname, 8883)
+    .WithClientId(clientId)
+    .WithCredentials("client1-authnID", "")  //use client authentication name in the username
+    .WithTls(new MqttClientOptionsBuilderTlsParameters()
+    {
+        UseTls = true,
+        Certificates = new X509Certificate2Collection(certificate)
+    })
+
+    .Build());
+
+Console.WriteLine($"Client Connected: {mqttClient.IsConnected} with CONNACK: {connAck.ResultCode}");
+
+mqttClient.ApplicationMessageReceivedAsync += async m => await Console.Out.WriteAsync($"Received message on topic: '{m.ApplicationMessage.Topic}' with content: '{m.ApplicationMessage.ConvertPayloadToString()}'\n\n");
+
+var suback = await mqttClient.SubscribeAsync("heroes/gotham");
+suback.Items.ToList().ForEach(s => Console.WriteLine($"subscribed to '{s.TopicFilter.Topic}' with '{s.ResultCode}'"));
+
+var counter = 0;
+
+while (true)
+{
+    var puback = await mqttClient.PublishStringAsync("heroes/gotham", $"hello world #{counter}!");
+    Console.WriteLine(puback.ReasonString);
+    await Task.Delay(5000);
+
+    counter++;
+
+}
--- steps.sh
+++ steps.sh
@@ -103,10 +103,58 @@ az eventgrid namespace create \
 -n $EVENT_GRID_MQTT_NS \
 --topic-spaces-configuration "{state:Enabled}"
 
+# Create a sample client certificate with step
+
+# 1. To create root and intermediate certificates, run the following command
+step ca init --deployment-type standalone \
+--name MqttAppSamplesCA \
+--dns localhost \
+--address 127.0.0.1:443 \
+--provisioner MqttAppSamplesCAProvisioner
+
+# Generated password: AmVBT=$5rpG7_Kw[<F\t(e_m5AS]u3fh
+
+# 2. Use the certificate authority (CA) files generated to create a certificate for the client
+step certificate create client1-authnID client1-authnID.pem \
+client1-authnID.key --ca /home/node/.step/certs/intermediate_ca.crt \
+--ca-key /home/node/.step/secrets/intermediate_ca_key \
+--no-password --insecure --not-after 2400h
+
+# 3. To view the thumbprint, run the step command.
+THUMBPRINT=$(step certificate fingerprint client1-authnID.pem)
+
 # Create client
-az eventgrid namespace client create -g {Resource Group} --namespace-name {Namespace Name} -n {Client Name} --authentication-name client1-authnID --client-certificate-authentication "{validationScheme:ThumbprintMatch,allowed-thumbprints:[Client Thumbprint]}"
+az eventgrid namespace client create \
+-g $RESOURCE_GROUP \
+--namespace-name $EVENT_GRID_MQTT_NS \
+-n batmovil \
+--authentication-name client1-authnID \
+--client-certificate-authentication "{validationScheme:ThumbprintMatch,allowed-thumbprints:[$THUMBPRINT]}"
 
+# Create topic space
+az eventgrid namespace topic-space create \
+-g $RESOURCE_GROUP \
+--namespace-name $EVENT_GRID_MQTT_NS \
+-n "autoslocos" \
+--topic-templates ['heroes/gotham']
 
+# Create permission bindings
+az eventgrid namespace permission-binding create \
+-g $RESOURCE_GROUP \
+--namespace-name $EVENT_GRID_MQTT_NS \
+-n publishers \
+--client-group-name '$all' \
+--permission publisher --topic-space-name "autoslocos"
+
+az eventgrid namespace permission-binding create \
+-g $RESOURCE_GROUP \
+--namespace-name $EVENT_GRID_MQTT_NS \
+-n subscribers \
+--client-group-name '$all' \
+--permission subscriber \
+--topic-space-name "autoslocos"
+
+# https://github.com/Azure-Samples/MqttApplicationSamples
 
 time az group delete -n $RESOURCE_GROUP --yes
 
